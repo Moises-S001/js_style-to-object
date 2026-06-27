c70fcb440f669dbaf600cff507ede24d9acc1a7a
moises <carlosmoises.s.s02@gmail.com>
2026-06-27 14:27:01 -0300

    solution task js_style-to-object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((codeCss) => codeCss.trim())
+    .filter((trimCodeCss) => trimCodeCss.includes(':'))
+    .reduce((stylesObjeto, atualLine) => {
+      const [chave, valor] = atualLine.split(':');
+
+      stylesObjeto[chave.trim()] = valor.trim();
+
+      return stylesObjeto;
+    }, {});
 }
 
 module.exports = convertToObject;
